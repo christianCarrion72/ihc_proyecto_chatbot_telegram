68f176109bcc71dff482d155e8171486142054c3
christianCarrion72 <christianco720@gmail.com>
2026-03-04 23:14:06 -0400

    feat [FASTAPI]: Conectar a la base de datos

--- telegram_bot_fastapi/app/main.py
+++ telegram_bot_fastapi/app/main.py
@@ -1,7 +1,14 @@
-from turtle import title
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI(title="Proyecto IHC - Chatbot")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Initializing database...")
+    init_db()
+    yield
+    print("Shutting down database...")
+
+app = FastAPI(title="Proyecto IHC - Chatbot", lifespan=lifespan)
 
 @app.get("/")
 def root():
