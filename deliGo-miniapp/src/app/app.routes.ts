d8556f7471cf60534f90855c6bbecec77b392f6f
christianCarrion72 <christianco720@gmail.com>
2026-03-10 06:42:05 -0400

    refactor [FRONT]: Configurar las rutas del front

--- deliGo-miniapp/src/app/app.routes.ts
+++ deliGo-miniapp/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'verificacion',
+    loadComponent: () =>
+      import('./verificacion-pago.component').then(
+        (m) => m.VerificacionPagoComponent
+      ),
+  },
+  {
+    path: 'ubicacion',
+    loadComponent: () =>
+      import('./mapa-ubicacion.component').then(
+        (m) => m.MapaUbicacionComponent
+      ),
+  },
+];
