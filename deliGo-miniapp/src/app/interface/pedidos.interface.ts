d4e9baecebc7966608952f3200a8bac11e4c36e3
christianCarrion72 <christianco720@gmail.com>
2026-03-10 06:40:42 -0400

    refactor [FRONT]: Crear interfaces para manejar las respuestas del backend

--- deliGo-miniapp/src/app/interface/pedidos.interface.ts
+++ deliGo-miniapp/src/app/interface/pedidos.interface.ts
@@ -10,3 +10,33 @@ export interface Categoria {
   id: number;
   nombre: string;
 }
+
+export interface DetalleParaPedidoPayload {
+  cantidad: number;
+  observacion: string;
+  plato_id: number;
+}
+
+export interface PedidoCompletoPayload {
+  total: number;
+  estado: string;
+  ubicacion_entrega: string;
+  precio_delivery: number;
+  chat_id: string;
+  nombre_usuario: string;
+  delivery_id: number | null;
+  detalles: DetalleParaPedidoPayload[];
+}
+
+export interface PedidoResponse {
+  id: number;
+  total: number;
+  estado: string;
+  ubicacion_entrega: string;
+  precio_delivery: number;
+  chat_id: string;
+  nombre_usuario: string;
+  delivery_id: number | null;
+  created_at: string;
+  updated_at: string;
+}
