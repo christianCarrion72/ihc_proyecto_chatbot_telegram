62bff25b3fd0534773965c2fa7bfcbbf1ca275bc
christianCarrion72 <christianco720@gmail.com>
2026-03-10 22:11:17 -0400

    refactor [MOBILE]: CAMBIO DE COLORES PRIMARIOS Y SECUNDARIOS

--- restaurant_delivery_app/lib/core/theme/app_colors.dart
+++ restaurant_delivery_app/lib/core/theme/app_colors.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
-  static const Color primary = Color(0xFFE53935);
-  static const Color secundary = Color(0xFFFFA726);
+  static const Color primary = Color(0xFFFFA726);
+  static const Color secundary = Color(0xFF30B19B);
   static const Color background = Color(0xFFF5F5F5);
   static const Color surface = Colors.white;
 
