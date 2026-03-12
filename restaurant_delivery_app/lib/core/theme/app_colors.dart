a969c82b9d562dabaf9a9eb69adb439773c8fb2b
christianCarrion72 <christianco720@gmail.com>
2026-03-12 03:40:37 -0400

    refactor [MOBILE]: Configurar los temas de los textos

--- restaurant_delivery_app/lib/core/theme/app_colors.dart
+++ restaurant_delivery_app/lib/core/theme/app_colors.dart
@@ -8,9 +8,9 @@ class AppColors {
   static const Color background = Color(0xFFF5F5F5);
   static const Color surface = Colors.white;
 
-  static const Color textPrimary = Color(0xFF212121);
-  static const Color textSecundary = Color(0xFF757575);
+  static const Color textPrimary = Color(0xFF000000);
+  static const Color textSecundary = Color(0xFF525151);
 
-  static const Color success = Color(0xFF43A047);
-  static const Color error = Color(0xFFD32F2F);
+  static const Color success = Color(0xFF40AC92);
+  static const Color error = Color(0xFFFE1C1C);
 }
